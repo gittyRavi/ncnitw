2b4a4230ced143e5c4346b37671bb41c59b7ef9b
রবি সাক্সেনা /  Рави Саксена <gittyRavi@users.noreply.github.com>
2017-10-18 18:21:04 +0530

    Add files via upload

---
+++ ABuJa.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1500);
+ 
+});
+/* Thanks to:
+  http://www.cantinanegrar.it/
+*/
---
+++ jquery.buttons.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#home").click(function(){
+		window.location = "https://gittyravi.github.io/ncnitw/index.html";		//index.html
+		return false;
+	});
+
+	$("#id1").click(function(){
+		window.location = "https://gittyravi.github.io/ncnitw/menu.html";		//menu.php
+		return false;
+	});
+	
+	$("#id2").click(function(){
+		window.location = "https://gittyravi.github.io/ncnitw/archive.html";	//archive.php
+		return false;
+	});
+	
+	$("#id3").click(function(){
+		window.location = "https://gittyravi.github.io/ncnitw/main_info.html";	//main_info.php
+		return false;
+	});
+	
+	$("#id4").click(function(){
+		window.location = "https://gittyravi.github.io/ncnitw/contacts.html";	//contacts.php
+		return false;
+	});
+});
